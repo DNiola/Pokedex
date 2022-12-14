e932f742026b36e84d074c6ced7bc864a4dfd724
davide n <davide.n@gmx.de>
2022-12-14 16:34:39 +0100

    add all Pokemons

--- script.js
+++ script.js
@@ -1,14 +1,48 @@
 let currentPokemon;
+let currentAPI;
 
 async function loadPokemon() {
-  let url = "https://pokeapi.co/api/v2/pokemon/charmander";
-  let response = await fetch(url);
-  currentPokemon = await response.json();
-  console.log("loaded pokemon", currentPokemon);
+  let APIs = "https://pokeapi.co/api/v2/pokemon/";
+  for (let t = 10; t < 11; t++) {
+    const API = `${APIs}${t}`;
+    console.log(API);
+    let response = await fetch(API);
+    currentPokemon = await response.json();
+    console.log("Pokemon:", currentPokemon);
+   pokemonFilter(currentPokemon, t)
+  }
+}
 
-  renderPokemonInfo();
+function pokemonFilter(currentPokemon, t) {
+
+  if (currentPokemon['types']['length'] == 2) {
+    document.getElementById(`pokemonTypesX${t}`).innerHTML += currentPokemon["types"][0]['type']['name'];
+    document.getElementById(`pokemonTypes${t}`).innerHTML += currentPokemon["types"][1]['type']['name'];
+} else {
+  document.getElementById(`pokemonTypesX${t}`).innerHTML += currentPokemon["types"][0]['type']['name'];
 }
-function renderPokemonInfo() {
-  document.getElementById("pokemonName").innerHTML = currentPokemon["name"];
-  document.getElementById("pokemonImage").src = currentPokemon["sprites"]["front_shiny"];
+  pokeDexHTML.innerHTML += renderAllPokemons(t, currentPokemon);
+  renderPokemonInfo(currentPokemon, t);
 }
+
+function renderPokemonInfo(currentPokemon, t) {
+  document.getElementById(`pokemonID${t}`).innerHTML += "#" + currentPokemon["id"];
+  document.getElementById(`pokemonName${t}`).innerHTML += currentPokemon["name"];
+  document.getElementById(`pokemonImage${t}`).src += currentPokemon["sprites"]["front_shiny"];
+}
+
+function renderAllPokemons(t) {
+  return `
+  <div id="pokemonContainer${t}"class="pokedex">
+    <div>
+      <span id="pokemonID${t}"></span>
+      <h1 id="pokemonName${t}"></h1>
+    </div>
+    <div class="infoContainer">
+      <img id="pokemonImage${t}">
+      <span style="color:black" id="pokemonTypes${t}"></span>
+      <span style="color:black" id="pokemonTypesX${t}"></span>;
+    </div>
+  </div>`;
+}
+
